cf9826c437a7a6c114a8571a7be21cffbcc5e629
Justuks <justinas.valutis@gmail.com>
2022-02-05 09:01:23 +0000

    skiping clock, will do other option

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+//we will initialise some innerHTML changes once we push press word in a header paragraph
+document.getElementById('press').onclick = function () {
+        document.getElementById('header-text').innerHTML = 'Please, complete your test below.';
+}
