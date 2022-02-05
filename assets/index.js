24d3f779fc8edb8f1ef301a9bc34bcf54b1cd5a0
Justuks <justinas.valutis@gmail.com>
2022-02-05 11:05:23 +0000

    complete questionnaire

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,24 @@
 document.getElementById('press').onclick = function () {
         document.getElementById('header-text').innerHTML = 'Please, complete your test below in order to accsess range finder.';
 
-/* now in this function, there is also an intention to open test questionnaire 
-upon pressing the word 'here' on the initial header patragraph
-document. Currently questionnaire is hidden with the help of CSS*/
-document.getElementById('test').style.visibility = 'visible';
+        /* now in this function, there is also an intention to open test questionnaire 
+        upon pressing the word 'here' on the initial header patragraph
+        document. Currently questionnaire is hidden with the help of CSS*/
+        document.getElementById('test').style.visibility = 'visible';
 }
 
+//work on questionnaire/make an array with correct answers
+let correctAnswers = ['port', 360, 1.85, 'nautical mile'];
+let button = document.querySelector('#check');
+button.addEventListener('click', function () {
+        let answers = document.querySelectorAll('#test input');
+        let i = 0;
+        for (let input of answers) {
+                if (input.value == correctAnswers[i]) {
+                        alert('good')
+                } else {
+                        document.getElementById('header-text').innerHTML = 'Please, try again.'
+                }
+                i++
+        }
+})
